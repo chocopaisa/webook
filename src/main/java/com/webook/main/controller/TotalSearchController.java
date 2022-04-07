5884d2db2fdd87acf16003836771e63a4fb4469f
hayeong216 <gkdud_dh@naver.com>
2022-04-07 16:21:49 +0900

    통합 검색 구현 완료, 상세페이지 상세이미지 삭제
    total_search. jsp : 도서, 리뷰 ajax 추가
    TotalSearchController : 한줄평 검색, 도서 및 리뷰 ajax  추가
    searchDAO, searchService : 책 검색 추가
    product_single : 도서 상세 이미지 삭제
    마이페이지 구매내역 : 사이드 바 추가

--- src/main/java/com/webook/main/controller/TotalSearchController.java
+++ src/main/java/com/webook/main/controller/TotalSearchController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.webook.community.service.CommunityService;
 import com.webook.domain.CommunityVO;
@@ -24,29 +25,59 @@ public class TotalSearchController {
 	@Autowired
 	private SearchService main;
 
-	
+
 	/*통합 검색*/
 	@RequestMapping("total_search.do")
 	public String totalSearch (ProductVO vo, String pnum, Model m) {
+
 		//한줄평 검색
-		/*
-		 * List<HashMap> reviews = main.getReviews(vo, pnum); // 리뷰 for(int idx =
-		 * reviews.size()-1; idx >= 0; idx--) { String content = (String)
-		 * reviews.get(idx).get("BOOKREPORT_CONTENT"); System.out.println(content); int
-		 * start = content.indexOf("<blockquote>"); if(start < 0) { continue; } int end
-		 * = content.indexOf("</blockquote>");
-		 * reviews.get(idx).put("BOOKREPORT_CONTENT",content.substring(start + 12,
-		 * end)); System.out.println(content.substring(start + 12, end)); }
-		 */
-		
+		List<HashMap> reviews = main.getReviews(vo, pnum); // 리뷰 
+		for(int idx = reviews.size()-1; idx >= 0; idx--) {
+			String content = (String) reviews.get(idx).get("BOOKREPORT_CONTENT");
+			System.out.println(content);
+			int start = content.indexOf("<blockquote>");
+			if(start < 0) {
+				continue;
+			}
+			int end = content.indexOf("</blockquote>");
+			reviews.get(idx).put("BOOKREPORT_CONTENT",content.substring(start + 12, end));
+			System.out.println(content.substring(start + 12, end));
+		}
+
 		// 도서 검색
-		List<ProductVO> result = product.genreKo(vo,pnum);
-		
-		//m.addAttribute("reviews", reviews);
+		List<ProductVO> result = main.bookSearch(vo,pnum);
+
+		m.addAttribute("reviews", reviews);
 		m.addAttribute("products", result);
-		
+
 		return "/total_search";
 	}
+
+
+	// 도서 ajax
+	@RequestMapping("bookList.do")
+	@ResponseBody
+	public List<ProductVO> bookAddBtn (ProductVO vo, String pnum) {
+		List<ProductVO> list = main.bookSearch(vo,pnum);
+		return list;
+	}
 	
+	@RequestMapping("reviewList.do")
+	@ResponseBody
+	public List<HashMap> reviewAddBtn (ProductVO vo, String pnum) {
+		List<HashMap> reviews = main.getReviews(vo, pnum); // 리뷰 
+		for(int idx = reviews.size()-1; idx >= 0; idx--) {
+			String content = (String) reviews.get(idx).get("BOOKREPORT_CONTENT");
+			System.out.println(content);
+			int start = content.indexOf("<blockquote>");
+			if(start < 0) {
+				continue;
+			}
+			int end = content.indexOf("</blockquote>");
+			reviews.get(idx).put("BOOKREPORT_CONTENT",content.substring(start + 12, end));
+			System.out.println(content.substring(start + 12, end));
+		}
+		return reviews;
+	}
 	
 }
